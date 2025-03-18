ef0120b75a4a3485889dd6c9a6b4879f7abee976
james-francis-mt <james.francis@madetech.com>
2025-03-18 23:02:44 +0000

    exit 0

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,13 @@
+import kotlin.system.exitProcess
+
 fun main() {
     while (true) {
         print("$ ")
 
         val input = readln()
+        if (input == "exit 0") {
+            exitProcess(0)
+        }
         println("$input: command not found")
     }
 }
