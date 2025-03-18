1d73ce59a9a0d7550b837903ab633d8832fbf5ae
james-francis-mt <james.francis@madetech.com>
2025-03-18 23:27:05 +0000

    echo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,15 @@ fun main() {
     while (true) {
         print("$ ")
 
-        val input = readln()
-        if (input == "exit 0") {
+        val command = readln()
+        val parts = command.split(" ", limit = 2)
+
+        if (command == "exit 0") {
             exitProcess(0)
+        } else if (parts[0] == "echo") {
+            println(parts[1])
+        } else {
+            println("$command: command not found")
         }
-        println("$input: command not found")
     }
 }
