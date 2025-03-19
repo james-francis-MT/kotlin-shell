cf03cafe81082f74ddb32c30651ba6cb53ed69d0
james-francis-mt <james.francis@madetech.com>
2025-03-19 00:10:46 +0000

    path

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import kotlin.system.exitProcess
 
+val pathList = System.getenv("PATH").split(":")
+
 fun main() {
     while (true) {
         print("$ ")
@@ -10,17 +12,22 @@ fun main() {
         when (parts[0]) {
             "exit" -> exitProcess(0)
             "echo" -> println(parts[1])
-            "type" -> handleType(parts)
+            "type" -> handleType(parts[1])
             else -> println("$command: command not found")
         }
     }
 }
 
-fun handleType(parts: List<String>) {
+fun handleType(arg: String) {
     val validCommands = setOf("echo", "exit", "type")
-    if (validCommands.contains(parts[1])) {
-        println("${parts[1]} is a shell builtin")
+    if (validCommands.contains(arg)) {
+        println("$arg is a shell builtin")
         return
     }
-    println("${parts[1]}: not found")
+    val match = pathList.find { it.contains(arg) }
+    if (match.toBoolean()) {
+        println("$arg is $match}")
+        return
+    }
+    println("${arg}: not found")
 }
