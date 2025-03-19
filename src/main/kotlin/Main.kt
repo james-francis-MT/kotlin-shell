d5f6482e168c942004fad16ecdd32b086c63744d
james-francis-mt <james.francis@madetech.com>
2025-03-19 00:44:13 +0000

    type directory

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
+import java.nio.file.Files
+import kotlin.io.path.Path
+import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.name
 import kotlin.system.exitProcess
 
-val pathList = System.getenv("PATH").split(":")
+val pathList = System.getenv("PATH").split(":").filter { Files.exists(Path(it)) }
 
 fun main() {
     while (true) {
@@ -24,10 +28,13 @@ fun handleType(arg: String) {
         println("$arg is a shell builtin")
         return
     }
-    val match = pathList.find { it.contains(arg) }
-    if (match.toBoolean()) {
-        println("$arg is $match}")
-        return
+    for (path in pathList) {
+        val filesAtPath = Path(path).listDirectoryEntries()
+        val match = filesAtPath.find { it.name == arg }
+        if (match != null) {
+            println("$arg is $match")
+            return
+        }
     }
     println("${arg}: not found")
 }
