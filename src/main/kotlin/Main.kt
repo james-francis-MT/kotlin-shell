11948efdb05bd81e202370c88d82ba857d8421db
james-francis-mt <james.francis@madetech.com>
2025-03-21 00:09:35 +0000

    run programs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.nio.file.Files
+import java.nio.file.Path
 import kotlin.io.path.Path
 import kotlin.io.path.listDirectoryEntries
 import kotlin.io.path.name
@@ -12,29 +13,53 @@ fun main() {
 
         val command = readln()
         val parts = command.split(" ", limit = 2)
+        val cmd = parts[0]
+        val args = if (parts.count() == 2) parts[1] else null
 
-        when (parts[0]) {
+        when (cmd) {
             "exit" -> exitProcess(0)
-            "echo" -> println(parts[1])
-            "type" -> handleType(parts[1])
-            else -> println("$command: command not found")
+            "echo" -> println(args)
+            "type" -> handleType(args)
+            else -> handleUnknown(cmd, args)
         }
     }
 }
 
-fun handleType(arg: String) {
+fun handleType(arg: String?) {
+    if (arg == null) {
+        println("Type command requires 1 argument")
+        return
+    }
     val validCommands = setOf("echo", "exit", "type")
     if (validCommands.contains(arg)) {
         println("$arg is a shell builtin")
         return
     }
+    when (val match = pathMatch(arg)) {
+        null -> println("${arg}: not found")
+        else -> println("$arg is ${match.toString()}")
+    }
+}
+
+fun handleUnknown(command: String, args: String?) {
+    val match = pathMatch(command)
+    if (match != null) {
+        ProcessBuilder(command, args)
+            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            .redirectError(ProcessBuilder.Redirect.INHERIT)
+            .start().waitFor()
+        return
+    }
+    println("$command: command not found")
+}
+
+fun pathMatch(arg: String): Path? {
     for (path in pathList) {
         val filesAtPath = Path(path).listDirectoryEntries()
         val match = filesAtPath.find { it.name == arg }
         if (match != null) {
-            println("$arg is $match")
-            return
+            return match
         }
     }
-    println("${arg}: not found")
+    return null
 }
